147d359a859a2304bc62a7e4c8759f247f25dd54
sasank1-54 <114579796+sasank1-54@users.noreply.github.com>
2022-10-07 15:37:07 +0530

    Add files via upload

---
+++ file.js
@@ -0,0 +1,22 @@
+
+//-------Project 24 (while loop)--------------------
+
+/*
+var count=0;
+while(count<5)
+{
+    let value = prompt("Enter the value");
+    console.log("Value : "+value);
+    count++;
+}
+*/
+
+//-----------Project 24 (for loop)-----------
+
+/*
+for(count=0;count<5;count++)
+{
+    let value = prompt("Enter the value");
+    console.log("Value : "+value);
+}
+*/
